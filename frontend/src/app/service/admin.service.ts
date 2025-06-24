1f22088dce4dd592a7e7b9492bc565620e738a4e
tirkandanow <zuzannalatas@gmail.com>
2025-06-24 19:29:30 +0200

    Reapply "tags and units can be added"
    This reverts commit 4707fb607ba27590ef41fe929176beb5e08fc6fa.

--- frontend/src/app/service/admin.service.ts
+++ frontend/src/app/service/admin.service.ts
@@ -17,8 +17,8 @@ export interface IngredientUnit {
   providedIn: 'root'
 })
 export class AdminService {
-  private apiUrl = 'http://localhost:8080/api';
-
+  private adminapiUrl = 'http://localhost:8080/api';
+  private apiUrl = 'http://localhost:8081/api'
   constructor(private http: HttpClient) {}
 
   private getAuthHeaders(): HttpHeaders {
@@ -42,42 +42,64 @@ export class AdminService {
   }
 
   updateIngredient(data: { id: number, name: string }): Observable<void> {
-    throw new Error("Not implemented")
+    return this.http.put<void>(`${this.adminapiUrl}/ingredient`, data, {
+      headers: this.getAuthHeaders(),
+      params: { id: data.id.toString() }
+    });
   }
 
   updateUnit(data: { id: number, name: string }): Observable<void> {
-    throw new Error("Not implemented")
+    return this.http.put<void>(`${this.adminapiUrl}/unit`, data, {
+      headers: this.getAuthHeaders(),
+      params: { id: data.id.toString() }
+    });
   }
 
   updateTag(data: { id: number, name: string }): Observable<void> {
-    throw new Error("Not implemented")
+    return this.http.put<void>(`${this.adminapiUrl}/tag`, data, {
+      headers: this.getAuthHeaders(),
+      params: { id: data.id.toString() }
+    });
   }
 
   deleteIngredient(id: number): Observable<void> {
-    throw new Error("Not implemented")
+    return this.http.delete<void>(`${this.adminapiUrl}/ingredient`, {
+      headers: this.getAuthHeaders(),
+      params: { id: id.toString() }
+    });
   }
 
   deleteUnit(id: number): Observable<void> {
-    throw new Error("Not implemented")
+    return this.http.delete<void>(`${this.adminapiUrl}/unit`, {
+      headers: this.getAuthHeaders(),
+      params: { id: id.toString() }
+    });
   }
 
   deleteTag(id: number): Observable<void> {
-    throw new Error("Not implemented");
+    return this.http.delete<void>(`${this.adminapiUrl}/tag`, {
+      headers: this.getAuthHeaders(),
+      params: { id: id.toString() }
+    });
   }
-  
+
   addIngredient(data: { name: string }): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}/ingredient`, data, { 
+    return this.http.post<string>(`${this.adminapiUrl}/ingredient`, data, {
       headers: this.getAuthHeaders(),
       responseType: 'text' as 'json'})
   }
 
   addUnit(data: { name: string }): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}/unit`, data, { headers: this.getAuthHeaders(),
-    responseType: 'text' as 'json' });
+    return this.http.post<string>(`${this.adminapiUrl}/unit`, data, {
+      headers: this.getAuthHeaders(),
+      responseType: 'text' as 'json'
+    });
   }
 
   addTag(data: { name: string }): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}/tag`, data, { headers: this.getAuthHeaders(),
-    responseType: 'text' as 'json' });
+    return this.http.post<string>(`${this.adminapiUrl}/tag`, data, {
+      headers: this.getAuthHeaders(),
+      responseType: 'text' as 'json'
+    });
   }
 }
